1ef5610283bdd49d9754fb9182659a796ba56d5b
AnuragBonde7 <146638549+AnuragBonde7@users.noreply.github.com>
2023-10-03 21:56:47 +0530

    Update LoginApplication.java

--- LoginApplication.java
+++ LoginApplication.java
@@ -35,7 +35,7 @@ class LoginApplication {
         // Display available trains (train number and name)
         System.out.println("\nAvailable Trains:");
         System.out.println("1. Train 440z - Express");
-        System.out.println("2. Train 540l - Superfast");
+        System.out.println("2. Train 540l - Swift");
         System.out.print("Enter the train number: ");
         int trainNumber = scanner.nextInt();
         scanner.nextLine(); // Consume the remaining newline character
